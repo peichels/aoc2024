bd3ab69da9fbf98ab085fe7f7c5ccbb51233db48
Peter Eichelsheim <peter.eichelsheim@ing.com>
2024-12-13 14:17:38 +0100

    Fixed day 7 part 1

--- day7/day7.go
+++ day7/day7.go
@@ -15,13 +15,14 @@ type line struct {
 }
 
 func GetSolutionPart1() (uint64, error) {
-	inputData, err := input.ReadInputFileAsStringList("./input/files/test_day7_1.txt")
+	inputData, err := input.ReadInputFileAsStringList("./input/files/day7_1.txt")
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 	}
 	var total uint64 = 0
+	var result uint64 = 0
 	var lines []line
-	var unreachableLines []line
+	//var unreachableLines []line
 	for _, lineString := range inputData {
 		outcome, _ := strconv.Atoi(strings.Split(lineString, ":")[0])
 		numbers := strings.Split(lineString, " ")[1:]
@@ -30,23 +31,23 @@ func GetSolutionPart1() (uint64, error) {
 			numberInt, _ := strconv.Atoi(number)
 			numbersInt = append(numbersInt, int32(numberInt))
 		}
-		timesTotal := 1
-		addTotal := 0
-		for _, number := range numbersInt {
-			timesTotal *= int(number)
-			addTotal += int(number)
-		}
-		if timesTotal < outcome {
-			fmt.Println("Can not reach outcome for Line: ", lineString, ": ", timesTotal)
-			unreachableLines = append(unreachableLines, line{uint64(outcome), numbersInt})
-			continue
-		}
-
-		if addTotal > outcome {
-			fmt.Println("Sum is larger than outcome for Line: ", lineString, ": ", addTotal)
-			unreachableLines = append(unreachableLines, line{uint64(outcome), numbersInt})
-			continue
-		}
+		//timesTotal := 1
+		//addTotal := 0
+		//for _, number := range numbersInt {
+		//	timesTotal *= int(number)
+		//	addTotal += int(number)
+		//}
+		//if timesTotal < outcome {
+		//	fmt.Println("Can not reach outcome for Line: ", lineString, ": ", timesTotal)
+		//	unreachableLines = append(unreachableLines, line{uint64(outcome), numbersInt})
+		//	continue
+		//}
+		//
+		//if addTotal > outcome {
+		//	fmt.Println("Sum is larger than outcome for Line: ", lineString, ": ", addTotal)
+		//	unreachableLines = append(unreachableLines, line{uint64(outcome), numbersInt})
+		//	continue
+		//}
 		lines = append(lines, line{uint64(outcome), numbersInt})
 
 	}
@@ -55,40 +56,53 @@ func GetSolutionPart1() (uint64, error) {
 		permutations := math.Pow(2, float64(len(line.numbers)-1))
 		fmt.Println(line.numbers)
 		fmt.Println("Permutations: ", permutations)
-		if line.outcome == 292 {
-			fmt.Println("292")
-		}
-		for i := 0; i < int(permutations); i++ {
-			// i = 0 len(line.numbers)-1 = 3 000
-			// i = 1 len(line.numbers)-1 = 3 001
-			// i = 2 len(line.numbers)-1 = 3 010
-			// i = 3 len(line.numbers)-1 = 3 011
-			// i = 4 len(line.numbers)-1 = 3 100
-			// i = 5 len(line.numbers)-1 = 3 101
-			// i = 6 len(line.numbers)-1 = 3 110
-			// i = 7 len(line.numbers)-1 = 3 111
-			var localTotal uint64 = 0
-			for j, number := range line.numbers {
-				if j == 0 {
-					localTotal = uint64(number)
-					continue
+
+		// iterate over all permutations
+		for perm := range int(permutations) {
+
+			for index, number := range line.numbers {
+				if index == 0 {
+					total = uint64(number)
 				}
-				if i&(1<<j) != 0 {
-					localTotal += uint64(number)
+				if index == len(line.numbers)-1 {
+					break
+				}
+				if perm&(1<<index) != 0 {
+					total *= uint64(line.numbers[index+1])
+					fmt.Printf("%d * %d,", number, line.numbers[index+1])
 				} else {
-					localTotal *= uint64(number)
+					total += uint64(line.numbers[index+1])
+					fmt.Printf("%d + %d,", number, line.numbers[index+1])
 				}
+
 			}
-			if localTotal == line.outcome {
-				total += localTotal
+			if total == line.outcome {
+				fmt.Println(" == ", total)
+				result += total
 				break
-			} else if localTotal > line.outcome {
-				continue
 			}
+			fmt.Println(" = ", total)
+
 		}
+		// 0: x[0] 0 x[1]
+		// 1: x[0] 1 x[1]
+
+		// 00: x[0] 0 x[1] 0 x[2]
+		// 01: x[0] 0 x[1] 1 x[2]
+		// 10: x[0] 1 x[1] 0 x[2]
+		// 11: x[0] 1 x[1] 1 x[2]
+
+		// 000: x[0] 0 x[1] 0 x[2] 0 x[3]
+		// 001: x[0] 0 x[1] 0 x[2] 1 x[3]
+		// 010: x[0] 0 x[1] 1 x[2] 0 x[3]
+		// 011: x[0] 0 x[1] 1 x[2] 1 x[3]
+		// 100: x[0] 1 x[1] 0 x[2] 0 x[3]
+		// 101: x[0] 1 x[1] 0 x[2] 1 x[3]
+		// 110: x[0] 1 x[1] 1 x[2] 0 x[3]
+
 	}
 
-	return total, nil
+	return result, nil
 }
 
 func GetSolutionPart2() (uint64, error) {
--- main.go
+++ main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"fmt"
-	"github.com/peichels/aoc2024/day12"
+	"github.com/peichels/aoc2024/day7"
 	"time"
 )
 
 func main() {
 	startTime := time.Now()
-	solution1, _ := day12.GetSolutionPart2()
+	solution1, _ := day7.GetSolutionPart1()
 	fmt.Println(solution1)
 	endTime := time.Now()
 	fmt.Println("Duration: ", endTime.Sub(startTime))
 
-	startTime = time.Now()
-	solution2, _ := day12.GetSolutionPart2()
-	fmt.Println(solution2)
-	endTime = time.Now()
-	fmt.Println("Duration: ", endTime.Sub(startTime))
+	//startTime = time.Now()
+	//solution2, _ := day12.GetSolutionPart2()
+	//fmt.Println(solution2)
+	//endTime = time.Now()
+	//fmt.Println("Duration: ", endTime.Sub(startTime))
 }
